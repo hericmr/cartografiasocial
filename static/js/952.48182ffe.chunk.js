750d7bee57d9319767c2825acd0293fd60b125b7
hericmr <seu-email@dominio.com>
2025-03-12 14:26:27 -0300

    Updates

--- static/js/952.48182ffe.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkmapa_santos=self.webpackChunkmapa_santos||[]).push([[952],{952:e=>{e.exports=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")}}}]);
-//# sourceMappingURL=952.48182ffe.chunk.js.map
---
+++ static/js/952.e4d2f464.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkmapa_santos=self.webpackChunkmapa_santos||[]).push([[952],{4952:e=>{e.exports=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")}}}]);
+//# sourceMappingURL=952.e4d2f464.chunk.js.map
